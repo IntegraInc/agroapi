7e9a0463c3a461bca82342317926adab04e99021
Julio Cesar <47247293+jcwisniewski@users.noreply.github.com>
2024-06-08 22:00:51 -0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -4,20 +4,10 @@ import cors from "cors";
 import clientsRoutes from "./routes/clientsRoutes";
 
 require("dotenv").config();
-const corsOptions = {
- origin: function (origin: any, callback: any) {
-  if (origin.endsWith(".vercel.app")) {
-   callback(null, true); // Se a origem está na lista branca ou termina com "vercel.app", permitir
-  } else {
-   callback(new Error("Not allowed by CORS")); // Caso contrário, rejeitar
-  }
- },
- credentials: true,
- optionSuccessStatus: 200,
-};
+
 const server = express();
 server.use(express.json());
-server.use(cors(corsOptions));
+server.use(cors({ origin: "*" }));
 const port = 3333;
 server.use(authRoutes);
 server.use(clientsRoutes);
